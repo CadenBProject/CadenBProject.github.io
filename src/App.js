55341de962490171eaa3e01e11a775711c83c022
Kabir Virji <kabir.virji@faire.com>
2021-12-25 15:30:25 -0800

    changed state to strings and added more branching

--- src/App.js
+++ src/App.js
@@ -2,57 +2,104 @@ import runningGif from './assets/running.gif';
 import './App.css';
 import React, { useState } from 'react';
 
-const StoryText = (props) => {
-
-  if (props.state === 0) {
-    return (
-      <div className="StoryText">
-      <h2>This is the text for the story</h2>
-      <p>
-        You can have different font sizes if you'd like. Like having the title for the section above and
-        more sentences here to explain the scenerio. Let's make sure we check how this looks on mobile too.
-      </p>
-    </div>
-    )
-  }
-
-  if (props.state === 1) {
-    return (
-      <div className="StoryText">
-      <h2>This is the next phase of the story</h2>
-      <p>
-        This is more text regarding the second phase of the story. Some nice story here :)
-      </p>
-    </div>
-    )
-  }
-
-}
-
-const StoryOptions = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      continue
-    </button>
-  )
-}
-
 function App() {
-  const [state, setState] = useState(0);
+  const [node, setNode] = useState("intro");
 
-  return (
-    <div className="App">
-      <header className="App-header">
+  if (node === "intro") {
+    return (
+      <div className="App">
+        <header className="App-header">
         <img src={runningGif} alt="logo" />
-        <StoryText state={state} />
-        <StoryOptions 
-          onClick={() => {
-            setState(state + 1)
-          }}
-        />
-      </header>
-    </div>
-  );
+        <div className="StoryText">
+          <h2>Welcome to my game</h2>
+          <p>
+            Click continue to begin ...
+          </p>
+          <button onClick={() => {setNode("start")}}>
+            continue
+          </button>
+        </div>
+        </header>
+      </div>
+    )
+  }
+
+  if (node === "start") {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <img src={runningGif} alt="logo" />
+        <div className="StoryText">
+          <h2>THIS IS THE FIRST STORY THAT EVERYTHING WILL BRANCH FROM</h2>
+          <p>
+            This is some more text explaining that story
+          </p>
+          <button onClick={() => {setNode("kill them")}}>
+            kill them
+          </button>
+          <button onClick={() => {setNode("eat them")}}>
+            eat them
+          </button>
+          <button onClick={() => {setNode("push them")}}>
+            push them
+          </button>
+        </div>
+        </header>
+      </div>
+    )
+  }
+
+  if (node === "kill them") {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <img src={runningGif} alt="logo" />
+        <div className="StoryText">
+          <h2>You killed them</h2>
+          <p>
+            Sorry now you're arrested
+          </p>
+          <button onClick={() => {setNode("option 1")}}>
+            option 1
+          </button>
+          <button onClick={() => {setNode("option 2")}}>
+            option 2
+          </button>
+          <button onClick={() => {setNode("option 3")}}>
+            option 3
+          </button>
+        </div>
+        </header>
+      </div>
+    )
+  }
+
+  if (node === "eat them") {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <img src={runningGif} alt="logo" />
+        <div className="StoryText">
+          <h2>You ate them</h2>
+          <p>
+            Sorry now you're a cannibal
+          </p>
+          <button onClick={() => {setNode("option 1")}}>
+            option 1
+          </button>
+          <button onClick={() => {setNode("option 2")}}>
+            option 2
+          </button>
+          <button onClick={() => {setNode("option 3")}}>
+            option 3
+          </button>
+        </div>
+        </header>
+      </div>
+    )
+  }
+
+
 }
 
 export default App;
