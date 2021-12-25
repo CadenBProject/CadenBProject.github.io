208126a60e7addd0196298b0236b23fcac4db3db
Kabir Virji <kabir.virji@faire.com>
2021-12-25 09:57:47 -0800

    added some dummy text and attempted conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import runningGif from './assets/running.gif';
 import './App.css';
+import React, { useState } from 'react';
+
+const StoryText = (props) => {
+  
+  return props.state === 0 ? (
+    <div className="StoryText">
+      <h2>This is the text for the story</h2>
+      <p>
+        You can have different font sizes if you'd like. Like having the title for the section above and
+        more sentences here to explain the scenerio. Let's make sure we check how this looks on mobile too.
+      </p>
+    </div>
+  ) : null
+}
 
 function App() {
+  const [state, setState] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={runningGif} alt="logo" />
+        <StoryText state={state} />
       </header>
     </div>
   );
