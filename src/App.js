9fb533a56e5e6ab5f9d3ce96cb0df6a61b1ad87d
Kabir Virji <kabir.virji@faire.com>
2021-12-25 15:03:02 -0800

    added button

--- src/App.js
+++ src/App.js
@@ -3,16 +3,38 @@ import './App.css';
 import React, { useState } from 'react';
 
 const StoryText = (props) => {
-  
-  return props.state === 0 ? (
-    <div className="StoryText">
+
+  if (props.state === 0) {
+    return (
+      <div className="StoryText">
       <h2>This is the text for the story</h2>
       <p>
         You can have different font sizes if you'd like. Like having the title for the section above and
         more sentences here to explain the scenerio. Let's make sure we check how this looks on mobile too.
       </p>
     </div>
-  ) : null
+    )
+  }
+
+  if (props.state === 1) {
+    return (
+      <div className="StoryText">
+      <h2>This is the next phase of the story</h2>
+      <p>
+        This is more text regarding the second phase of the story. Some nice story here :)
+      </p>
+    </div>
+    )
+  }
+
+}
+
+const StoryOptions = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      continue
+    </button>
+  )
 }
 
 function App() {
@@ -23,6 +45,11 @@ function App() {
       <header className="App-header">
         <img src={runningGif} alt="logo" />
         <StoryText state={state} />
+        <StoryOptions 
+          onClick={() => {
+            setState(state + 1)
+          }}
+        />
       </header>
     </div>
   );
